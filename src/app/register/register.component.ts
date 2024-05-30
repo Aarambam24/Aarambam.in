f903132f14f2e06f51c8b4179fb9bf69b2d376d5
bandaruaneesh <aneeshbandaru@gmail.com>
2024-05-30 19:57:32 +0530

    Register Component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -5,10 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-<<<<<<< HEAD
-export class LoginComponent {
-=======
 export class RegisterComponent {
->>>>>>> 4e9ce092b99b88137cf84d189f6181b34040ec82
 
 }
